b34d29032225318e27a1f3c9ca2455f0a8906aae
cascades-sjtu <cascades@sjtu.edu.cn>
2024-02-04 22:33:56 +0800

    add map script

---
+++ _homepage/get_map.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# get map as image from Google Map API
+curl "https://maps.googleapis.com/maps/api/staticmap?\
+key=${GOOGLE_MAP_API_KEY}&\
+center=20,0&\
+zoom=1&\
+size=500x250&\
+scale=2&\
+markers=size:tiny|Melbroune|Shenzhen" --output ./assets/img/map.png
